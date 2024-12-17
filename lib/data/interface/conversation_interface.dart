49bcc1b92d607cfa43a42c26ddf567076b432c3f
Mahmoud Fawzee <mahmoudfawzee1511@gmail.com>
2024-12-18 00:52:49 +0200

    fix pubspec.yaml issues

--- lib/data/interface/conversation_interface.dart
+++ lib/data/interface/conversation_interface.dart
@@ -2,8 +2,10 @@ import 'package:ai_assistant_app/data/models/conversation.dart';
 
 abstract class ConversationInterface {
   Future<int> startNewConversation(Conversation conversation);
-  Future<int> updateConversation(
-      {required int oldId, required Conversation conversation});
+  Future<int> updateConversation({
+    required int oldId,
+    required Conversation conversation,
+  });
   Future<int> deleteConversation(int id);
   Future<bool> deleteConversationMessages(int conversationId);
   Future<List<Conversation>> getConversation();
